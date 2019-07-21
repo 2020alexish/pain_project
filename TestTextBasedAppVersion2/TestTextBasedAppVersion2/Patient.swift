d3e883cfe8f7403bfa34abe5fbd719e3f4237ad1
Alexis <2020alexish@chapin.edu>
2019-07-21 15:19:01 -0400

    fixed intializers

--- TestTextBasedAppVersion2/TestTextBasedAppVersion2/Patient.swift
+++ TestTextBasedAppVersion2/TestTextBasedAppVersion2/Patient.swift
@@ -7,25 +7,13 @@ class Patient{
     var typesurgery: String
     var physician: String
     var DOB: String
-    let surveyanswers = [String]()
-    init(patientName: String, surgeryType: String, patientPhysician: String, DateOfBirth: String, patientSurgeries: String){
-  
-        let surveyanswers = []
-    init(patientName: String, DateOfBirth: String, patientSurgeries: String){
+    var surveyanswers: [String]
+    init(patientName: String, patientPhysician: String, DateOfBirth: String, patientSurgeries: String, surveyanswers: [String]){
         self.name = patientName
         self.DOB = DateOfBirth
         self.typesurgery = patientSurgeries
-
-    }
-    static var patients = [Patient]()
-    
-    init() {
-        Patient.patients.append(self)
-    }
-    
-    init(name: String) {
-        self.name = name
-        Patient.patients.append(self)
+        self.physician = patientPhysician
+        self.surveyanswers = surveyanswers
     }
 
 }
@@ -35,11 +23,12 @@ class Survey: Patient{
     var medication:[String]
     var painLevel:Int
 
-    init(patientSymptom: [String],  patientMeds: [String],  patientPainLevel: Int, patientName: String, DateOfBirth: String, patientSurgeries: String) {
+    init(patientSymptom: [String],  patPhysician: String, patientMeds: [String],  patientPainLevel: Int, patientName: String, DateOfBirth: String, patientSurgeries: String, patSurvAnswer: [String]) {
         self.symptoms = patientSymptom
         self.medication = patientMeds
         self.painLevel = patientPainLevel
-        Patient.init(patientName, DateOfBirth, patientSurgeries)
+        super.init(patientName: patientName, patientPhysician: patPhysician, DateOfBirth: DateOfBirth, patientSurgeries: patientSurgeries, surveyanswers: patSurvAnswer)
+
     }
 }
 
