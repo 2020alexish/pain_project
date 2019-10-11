8f64528eab0673ec87f2b982d389c74c37eaa544
Alexis <2020alexish@chapin.edu>
2019-10-11 14:53:19 -0400

    added extraPatient

---
+++ TestTextBasedAppVersion2/TestTextBasedAppVersion2/extraPatient.swift
@@ -0,0 +1,38 @@
+class Patient{
+    var name: String
+    var typesurgery: String
+    var physician: String
+    var DOB: date
+    let surveyanswers = []
+    init(patientName: String, DateOfBirth: Date, patientSurgeries: String){
+        self.name = patientName
+        self.DOB = DateOfBirth
+        self.typesurgery = patientSurgeries
+    
+    }
+
+}
+
+class Survey: Patient{
+    var symptoms:String
+    var medication:String
+    var painLevel:Int
+    
+    init(patientSymptom: [],  patientMeds: [],  patientPainLevel: Int, patientName: String, DateOfBirth: Date, patientSurgeries: String) {
+        self.symptoms = patientSymptom
+        self.medication = patientMeds   
+        self.painLevel = patientPainLevel
+        super.init(patientName, DateOfBirth, patientSurgeries)
+    }
+}
+
+
+
+let patientList = []
+
+func addPatient(){
+    patientList.append(Patient)
+}
+func removePatient(){
+    patientList.remove(Patient)
+}
